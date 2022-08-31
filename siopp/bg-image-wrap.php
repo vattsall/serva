826939cdbbad61fba8ab40db2767c579d47c498f
vattsall <69675188+vattsall@users.noreply.github.com>
2022-08-31 23:32:59 +0530

    Add files via upload

---
+++ siopp/bg-image-wrap.php
@@ -0,0 +1,105 @@
+<div class="bgded" style="background-image:url('images/demo/backgrounds/serva_1.png'); 
+    background-position: center; 
+    background-size: cover;
+    opacity: 2;"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper overlay row0" style="background-color: black; opacity: 0.7;">
+    <div id="topbar" class="hoc clear" >
+      <div class="fl_left"> 
+        <!-- ###### ########################################################################################## -->
+        <ul class="nospace">
+          <li><i class="fas fa-phone rgtspace-5"></i> +91 9687458110</li>
+          <li><i class="far fa-envelope rgtspace-5"></i> info@serva.com</li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <div class="fl_right"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+          <li><a href="index.html"><i class="fas fa-home"></i></a></li>
+          <li><a href="#" title="Help Centre"><i class="far fa-life-ring"></i></a></li>
+          <li><a href="#" title="Login"><i class="fas fa-sign-in-alt"></i></a></li>
+          <li><a href="#" title="Sign Up"><i class="fas fa-edit"></i></a></li>
+          <li id="searchform">
+            <div>
+              <form action="#" method="post">
+                <fieldset>
+                  <legend>Quick Search:</legend>
+                  <input type="text" placeholder="Enter search term&hellip;">
+                  <button type="submit"><i class="fas fa-search"></i></button>
+                </fieldset>
+              </form>
+            </div>
+          </li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1" style="background-color: black; opacity: 0.8;">
+    <header id="header" class="hoc clear">
+      <div id="logo" class="fl_left"> 
+        <!-- ################################################################################################ -->
+        <h1 class="logoname"><a href="index.html">S<span>erva</span></a></h1>
+        <!-- ################################################################################################ -->
+      </div>
+      <nav id="mainav" class="fl_right"> 
+        <!-- ################################################################################################ -->
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a class="drop" href="#">Pages</a>
+            <ul>
+              <li><a href="pages/gallery.html">Gallery</a></li>
+              <li><a href="pages/full-width.html">Full Width</a></li>
+              <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+              <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+              <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+              <li><a href="pages/font-icons.html">Font Icons</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Dropdown</a>
+            <ul>
+              <li><a href="#">Level 2</a></li>
+              <li><a class="drop" href="#">Level 2 + Drop</a>
+                <ul>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Level 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Link Text</a></li>
+          <li><a href="#">Link Text</a></li>
+          <li><a href="#">Link Text</a></li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </nav>
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper overlay">
+    <div id="pageintro" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <article>
+        
+        <h3 class="heading">SERVA</h3>
+        <p>we the people from serva welcome you.</p>
+        <footer>
+          <ul class="nospace inline pushright">
+            <li><a class="btn inverse" href="forms.html">Join us </a></li>
+            <li><a class="btn inverse" href="#">About us</a></li>
+          </ul>
+        </footer>
+      </article>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+</div>
